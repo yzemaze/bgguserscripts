3b1e1005825ef85e39c945b05883f006b7d80934
yzemaze <33390595+yzemaze@users.noreply.github.com>
2020-03-08 15:56:37 +0100

    Update bggUsernameFormatter-ajax.user.js

--- bggUsernameFormatter-ajax.user.js
+++ bggUsernameFormatter-ajax.user.js
@@ -7,7 +7,7 @@
 // @supportURL  https://github.com/yzemaze/bgguserscripts/issues
 // @downloadURL https://github.com/yzemaze/bgguserscripts/raw/master/bggUsernameFormatter-ajax.user.js
 // @updateURL   https://github.com/yzemaze/bgguserscripts/raw/master/bggUsernameFormatter-ajax.user.js
-// @namespace		https://github.com/yzemaze/bgguserscripts
+// @namespace   https://github.com/yzemaze/bgguserscripts
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @run-at      document-idle
